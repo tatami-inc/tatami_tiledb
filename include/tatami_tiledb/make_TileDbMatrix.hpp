cf3fb4c3eae7992e076dca5afb85af44043faa3a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-05-29 23:02:29 -0700

    Fixed up the documentation for make_* overloads.

--- include/tatami_tiledb/make_TileDbMatrix.hpp
+++ include/tatami_tiledb/make_TileDbMatrix.hpp
@@ -14,16 +14,18 @@
 namespace tatami_tiledb {
 
 /**
- * @brief Make a TileDB-backed matrix.
- *
  * @tparam Value_ Numeric type of the matrix value.
  * @tparam Index_ Integer type for the row/column indices.
  * @tparam transpose_ Whether to transpose the on-disk data upon loading.
  * By default, this is `false`, so the first dimension corresponds to rows and the second dimension corresponds to columns.
  * If `true`, the first dimension corresponds to columns and the second dimension corresponds to rows.
  *
- * As its name suggests, this function creates a numeric matrix stored as a 2-dimensional dense or sparse TileDB array.
- * It will automatically choose the most appropriate representation based on the storage format.
+ * @param uri File path (or some other appropriate location) of the TileDB array.
+ * @param attribute Name of the attribute containing the data of interest.
+ * @param options Further options.
+ *
+ * Create a `tatami::Matrix` representation from a 2-dimensional dense or sparse TileDB array.
+ * This function will automatically choose the most appropriate representation based on the storage format.
  * For dense on-disk arrays, a `TileDbDenseMatrix` is constructed; otherwise a `TileDbSparseMatrix` is returned.
  */
 template<typename Value_, typename Index_, bool transpose_ = false>
@@ -44,6 +46,19 @@ std::shared_ptr<tatami::Matrix<Value_, Index_> > make_TileDbMatrix(std::string u
     return output; 
 }
 
+/**
+ * @tparam Value_ Numeric type of the matrix value.
+ * @tparam Index_ Integer type for the row/column indices.
+ * @tparam transpose_ Whether to transpose the on-disk data upon loading.
+ * By default, this is `false`, so the first dimension corresponds to rows and the second dimension corresponds to columns.
+ * If `true`, the first dimension corresponds to columns and the second dimension corresponds to rows.
+ *
+ * @param uri File path (or some other appropriate location) of the TileDB array.
+ * @param attribute Name of the attribute containing the data of interest.
+ *
+ * Create a `tatami::Matrix` representation from a 2-dimensional dense or sparse TileDB array.
+ * Unlike its overload, this function will use the default settings in `TileDbOptions`.
+ */
 template<typename Value_, typename Index_, bool transpose_ = false>
 std::shared_ptr<tatami::Matrix<Value_, Index_> > make_TileDbMatrix(std::string uri, std::string attribute) {
     return make_TileDbMatrix<Value_, Index_, transpose_>(std::move(uri), std::move(attribute), TileDbOptions());
