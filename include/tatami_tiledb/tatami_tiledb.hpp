53e2a4f12a763c6c6990b5f86bab7724514b16c7
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-05-29 22:34:52 -0700

    Added an umbrella header for easy include.

---
+++ include/tatami_tiledb/tatami_tiledb.hpp
@@ -0,0 +1,20 @@
+#ifndef TATAMI_TILEDB_HPP
+#define TATAMI_TILEDB_HPP
+
+#include "TileDbDenseMatrix.hpp"
+#include "TileDbSparseMatrix.hpp"
+#include "TileDbOptions.hpp"
+#include "make_TileDbMatrix.hpp"
+
+/**
+ * @file tatami_tiledb.hpp
+ * @brief Umbrella header for TileDB representations.
+ */
+
+/**
+ * @brief **tatami** bindings for TileDB matrices.
+ * @namespace tatami_tiledb
+ */
+namespace tatami_tiledb {}
+
+#endif
