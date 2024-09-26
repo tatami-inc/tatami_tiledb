85000735ad034dc73891e7ef355da8d2aa8863c1
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-09-26 09:11:06 -0700

    Restored the typed vector.

--- include/tatami_tiledb/utils.hpp
+++ include/tatami_tiledb/utils.hpp
@@ -18,18 +18,91 @@ struct Components{
     tiledb::Array array;
 };
 
-class VariableDimension {
+// Handling conversion from TileDB's storage type to our desired in-memory
+// type - unlike HDF5, TileDB doesn't do this for us. 
+class VariablyTypedVector {
 public:
-    VariableDimension(const tiledb::Dimension& dim) : my_type(dim.type()) {
+    VariablyTypedVector(tiledb_datatype_t type, size_t len) : my_type(type) {
         switch (my_type) {
-            case TILEDB_INT8: populate(dim, my_i8_start, my_i8_end, my_i8_tile); break;
-            case TILEDB_UINT8: populate(dim, my_u8_start, my_u8_end, my_u8_tile); break;
-            case TILEDB_INT16: populate(dim, my_i16_start, my_i16_end, my_i16_tile); break;
-            case TILEDB_UINT16: populate(dim, my_u16_start, my_u16_end, my_u16_tile); break;
-            case TILEDB_INT32: populate(dim, my_i32_start, my_i32_end, my_i32_tile); break;
-            case TILEDB_UINT32: populate(dim, my_u32_start, my_u32_end, my_u32_tile); break;
-            case TILEDB_INT64: populate(dim, my_i64_start, my_i64_end, my_i64_tile); break;
-            case TILEDB_UINT64: populate(dim, my_u64_start, my_u64_end, my_u64_tile); break;
+            case TILEDB_CHAR:    my_char.resize(len); break;
+            case TILEDB_INT8:    my_i8.resize(len);   break;
+            case TILEDB_UINT8:   my_u8.resize(len);   break;
+            case TILEDB_INT16:   my_i16.resize(len);  break;
+            case TILEDB_UINT16:  my_u16.resize(len);  break;
+            case TILEDB_INT32:   my_i32.resize(len);  break;
+            case TILEDB_UINT32:  my_u32.resize(len);  break;
+            case TILEDB_INT64:   my_i64.resize(len);  break;
+            case TILEDB_UINT64:  my_u64.resize(len);  break;
+            case TILEDB_FLOAT32: my_f32.resize(len);  break;
+            case TILEDB_FLOAT64: my_f64.resize(len);  break;
+            default: throw std::runtime_error("unknown TileDB datatype '" + std::to_string(type) + "'");
+        }
+    }
+
+public:
+    void set(tiledb::Query& query, const std::string& name) {
+        switch (my_type) {
+            case TILEDB_CHAR:    query.set_data_buffer(name, my_char); break;
+            case TILEDB_INT8:    query.set_data_buffer(name, my_i8);   break;
+            case TILEDB_UINT8:   query.set_data_buffer(name, my_u8);   break;
+            case TILEDB_INT16:   query.set_data_buffer(name, my_i16);  break;
+            case TILEDB_UINT16:  query.set_data_buffer(name, my_u16);  break;
+            case TILEDB_INT32:   query.set_data_buffer(name, my_i32);  break;
+            case TILEDB_UINT32:  query.set_data_buffer(name, my_u32);  break;
+            case TILEDB_INT64:   query.set_data_buffer(name, my_i64);  break;
+            case TILEDB_UINT64:  query.set_data_buffer(name, my_u64);  break;
+            case TILEDB_FLOAT32: query.set_data_buffer(name, my_f32);  break;
+            case TILEDB_FLOAT64: query.set_data_buffer(name, my_f64);  break;
+            default: break;
+        }
+    }
+
+    template<typename Value_>
+    void copy(size_t offset, size_t len, Value_* dest) const {
+        switch (my_type) {
+            case TILEDB_CHAR:    std::copy_n(my_char.begin() + offset, len, dest); break;
+            case TILEDB_INT8:    std::copy_n(my_i8.begin()   + offset, len, dest); break;
+            case TILEDB_UINT8:   std::copy_n(my_u8.begin()   + offset, len, dest); break;
+            case TILEDB_INT16:   std::copy_n(my_i16.begin()  + offset, len, dest); break;
+            case TILEDB_UINT16:  std::copy_n(my_u16.begin()  + offset, len, dest); break;
+            case TILEDB_INT32:   std::copy_n(my_i32.begin()  + offset, len, dest); break;
+            case TILEDB_UINT32:  std::copy_n(my_u32,         + offset, len, dest); break;
+            case TILEDB_INT64:   std::copy_n(my_i64.begin()  + offset, len, dest); break;
+            case TILEDB_UINT64:  std::copy_n(my_u64.begin()  + offset, len, dest); break;
+            case TILEDB_FLOAT32: std::copy_n(my_f32.begin()  + offset, len, dest); break;
+            case TILEDB_FLOAT64: std::copy_n(my_f64.begin()  + offset, len, dest); break;
+            default: break;
+        }
+    }
+
+private:
+    tiledb_datatype_t my_type = TILEDB_INT32;
+
+    std::vector<char> my_char;
+    std::vector<int8_t> my_i8;
+    std::vector<uint8_t> my_u8;
+    std::vector<int16_t> my_i16;
+    std::vector<uint16_t> my_u16;
+    std::vector<int32_t> my_i32;
+    std::vector<uint32_t> my_u32;
+    std::vector<int64_t> my_i64;
+    std::vector<uint64_t> my_u64;
+    std::vector<float> my_f32;
+    std::vector<double> my_f64;
+};
+
+class VariablyTypedDimension {
+public:
+    VariablyTypedDimension(const tiledb::Dimension& dim) : my_type(dim.type()) {
+        switch (my_type) {
+            case TILEDB_INT8:    populate(dim, my_i8_start,  my_i8_end,  my_i8_tile);  break;
+            case TILEDB_UINT8:   populate(dim, my_u8_start,  my_u8_end,  my_u8_tile);  break;
+            case TILEDB_INT16:   populate(dim, my_i16_start, my_i16_end, my_i16_tile); break;
+            case TILEDB_UINT16:  populate(dim, my_u16_start, my_u16_end, my_u16_tile); break;
+            case TILEDB_INT32:   populate(dim, my_i32_start, my_i32_end, my_i32_tile); break;
+            case TILEDB_UINT32:  populate(dim, my_u32_start, my_u32_end, my_u32_tile); break;
+            case TILEDB_INT64:   populate(dim, my_i64_start, my_i64_end, my_i64_tile); break;
+            case TILEDB_UINT64:  populate(dim, my_u64_start, my_u64_end, my_u64_tile); break;
             case TILEDB_FLOAT32: populate(dim, my_f32_start, my_f32_end, my_f32_tile); break;
             case TILEDB_FLOAT64: populate(dim, my_f64_start, my_f64_end, my_f64_tile); break;
             default: throw std::runtime_error("unknown TileDB datatype '" + std::to_string(type) + "'");
@@ -41,14 +114,14 @@ public:
     Index_ extent() const {
         Index_ output = 0;
         switch (my_type) {
-            case TILEDB_INT8: output = define_extent<Index_>(my_i8_start, my_i8_end); break;
-            case TILEDB_UINT8: output = define_extent<Index_>(my_u8_start, my_u8_end); break;
-            case TILEDB_INT16: output = define_extent<Index_>(my_i16_start, my_i16_end); break;
-            case TILEDB_UINT16: output = define_extent<Index_>(my_u16_start, my_u16_end); break;
-            case TILEDB_INT32: output = define_extent<Index_>(my_i32_start, my_i32_end); break;
-            case TILEDB_UINT32: output = define_extent<Index_>(my_u32_start, my_u32_end); break;
-            case TILEDB_INT64: output = define_extent<Index_>(my_i64_start, my_i64_end); break;
-            case TILEDB_UINT64: output = define_extent<Index_>(my_u64_start, my_u64_end); break;
+            case TILEDB_INT8:    output = define_extent<Index_>(my_i8_start,  my_i8_end);  break;
+            case TILEDB_UINT8:   output = define_extent<Index_>(my_u8_start,  my_u8_end);  break;
+            case TILEDB_INT16:   output = define_extent<Index_>(my_i16_start, my_i16_end); break;
+            case TILEDB_UINT16:  output = define_extent<Index_>(my_u16_start, my_u16_end); break;
+            case TILEDB_INT32:   output = define_extent<Index_>(my_i32_start, my_i32_end); break;
+            case TILEDB_UINT32:  output = define_extent<Index_>(my_u32_start, my_u32_end); break;
+            case TILEDB_INT64:   output = define_extent<Index_>(my_i64_start, my_i64_end); break;
+            case TILEDB_UINT64:  output = define_extent<Index_>(my_u64_start, my_u64_end); break;
             case TILEDB_FLOAT32: output = define_extent<Index_>(my_f32_start, my_f32_end); break;
             case TILEDB_FLOAT64: output = define_extent<Index_>(my_f64_start, my_f64_end); break;
             default: break;
@@ -60,14 +133,14 @@ public:
     Index_ tile() const {
         Index_ output = 0;
         switch (my_type) {
-            case TILEDB_INT8: output = my_i8_tile; break;
-            case TILEDB_UINT8: output = my_u8_tile; break;
-            case TILEDB_INT16: output = my_i16_tile; break;
-            case TILEDB_UINT16: output = my_u16_tile; break;
-            case TILEDB_INT32: output = my_i32_tile; break;
-            case TILEDB_UINT32: output = my_u32_tile; break;
-            case TILEDB_INT64: output = my_i64_tile; break;
-            case TILEDB_UINT64: output = my_u64_tile; break;
+            case TILEDB_INT8:    output = my_i8_tile;  break;
+            case TILEDB_UINT8:   output = my_u8_tile;  break;
+            case TILEDB_INT16:   output = my_i16_tile; break;
+            case TILEDB_UINT16:  output = my_u16_tile; break;
+            case TILEDB_INT32:   output = my_i32_tile; break;
+            case TILEDB_UINT32:  output = my_u32_tile; break;
+            case TILEDB_INT64:   output = my_i64_tile; break;
+            case TILEDB_UINT64:  output = my_u64_tile; break;
             case TILEDB_FLOAT32: output = my_f32_tile; break;
             case TILEDB_FLOAT64: output = my_f64_tile; break;
             default: break;
@@ -78,16 +151,16 @@ public:
     template<typename Index_>
     void add_range(tiledb::Query& query, Index_ start, Index_ length) const {
         switch (my_type) {
-            case TILEDB_INT8: output = add(query, my_i8_start, start, length); break;
-            case TILEDB_UINT8: output = add(query, my_u8_start, start, length); break;
-            case TILEDB_INT16: output = add(query, my_i16_start, start, length); break;
-            case TILEDB_UINT16: output = add(query, my_u16_start, start, length); break;
-            case TILEDB_INT32: output = add(query, my_i32_start, start, length); break;
-            case TILEDB_UINT32: output = add(query, my_u32_start, start, length); break;
-            case TILEDB_INT64: output = add(query, my_i64_start, start, length); break;
-            case TILEDB_UINT64: output = add(query, my_u64_start, start, length); break;
-            case TILEDB_FLOAT32: output = add(query, my_f32_start, start, length); break;
-            case TILEDB_FLOAT64: output = add(query, my_f64_start, start, length); break;
+            case TILEDB_INT8:    add(query, my_i8_start,  start, length);  break;
+            case TILEDB_UINT8:   add(query, my_u8_start,  start, length);  break;
+            case TILEDB_INT16:   add(query, my_i16_start, start, length); break;
+            case TILEDB_UINT16:  add(query, my_u16_start, start, length); break;
+            case TILEDB_INT32:   add(query, my_i32_start, start, length); break;
+            case TILEDB_UINT32:  add(query, my_u32_start, start, length); break;
+            case TILEDB_INT64:   add(query, my_i64_start, start, length); break;
+            case TILEDB_UINT64:  add(query, my_u64_start, start, length); break;
+            case TILEDB_FLOAT32: add(query, my_f32_start, start, length); break;
+            case TILEDB_FLOAT64: add(query, my_f64_start, start, length); break;
             default: break;
         }
     }
